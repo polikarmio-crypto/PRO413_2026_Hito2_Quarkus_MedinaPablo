41ed78628483fd90942af1b0bbff04d59ade563d
polikarmio-crypto <polikarmio3@gmail.com>
2026-09-05 08:37:10 -0400

    test: agregar pruebas unitarias para endpoints de peliculas

---
+++ src/test/java/com/evaluacion/resource/PeliculaResourceIT.java
@@ -0,0 +1,8 @@
+package com.evaluacion.resource;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class PeliculaResourceIT extends PeliculaResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ src/test/java/com/evaluacion/resource/PeliculaResourceTest.java
@@ -0,0 +1,43 @@
+package com.evaluacion.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
+@QuarkusTest
+class PeliculaResourceTest {
+
+    @Test
+    void testListarPeliculasEndpoint() {
+        given()
+          .when().get("/api/peliculas")
+          .then()
+             .statusCode(200)
+             .contentType(ContentType.JSON)
+             .body("$", notNullValue());
+    }
+
+    @Test
+    void testRegistrarPeliculaEndpoint() {
+        String jsonNuevaPelicula = "{"
+                + "\"titulo\": \"Matrix\","
+                + "\"director\": \"Lana y Lilly Wachowski\","
+                + "\"anio\": 1999,"
+                + "\"genero\": \"Ciencia Ficción\","
+                + "\"sinopsis\": \"Un programador descubre la verdadera naturaleza de su realidad.\""
+                + "}";
+
+        given()
+          .contentType(ContentType.JSON)
+          .body(jsonNuevaPelicula)
+          .when().post("/api/peliculas")
+          .then()
+             .statusCode(201)
+             .contentType(ContentType.JSON)
+             .body("titulo", is("Matrix"))
+             .body("director", is("Lana y Lilly Wachowski"));
+    }
+}
